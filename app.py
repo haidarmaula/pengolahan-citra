eb0be273334dbaa58f934345c6838c62498ab147
Snasset <dheril.oct@gmail.com>
2024-10-29 19:42:48 +0700

    feat: add hex table and histogram

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, scrolledtext
 from PIL import Image, ImageTk
 import colorsys  # Digunakan untuk konversi RGB ke HSL
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Konstanta untuk batas ukuran tampilan gambar
 MAX_WIDTH = 500
@@ -49,7 +51,7 @@ class ImageRGBViewerApp:
         self.entry_col.grid(row=1, column=1)
 
         # Tombol untuk mendapatkan nilai RGB
-        btn_get_rgb = tk.Button(frame_input, text="Dapatkan RGB dan HSL", command=self.get_rgb_hsl_at_coordinates)
+        btn_get_rgb = tk.Button(frame_input, text="Dapatkan RGB, HSL, Hex", command=self.get_rgb_hsl_at_coordinates)
         btn_get_rgb.grid(row=2, column=0, columnspan=2, pady=5)
 
         # Label untuk menampilkan nilai RGB dan HSL
@@ -57,6 +59,8 @@ class ImageRGBViewerApp:
         self.label_rgb.pack()
         self.label_hsl = tk.Label(self.root, text="HSL pada (Y, X): -")
         self.label_hsl.pack()
+        self.label_hex = tk.Label(self.root, text="Hex pada (Y, X): -")
+        self.label_hex.pack()
 
         # Tombol untuk melihat semua nilai RGB
         btn_view_all_rgb = tk.Button(self.root, text="Lihat Semua Nilai RGB", command=self.view_all_rgb)
@@ -65,6 +69,18 @@ class ImageRGBViewerApp:
         # Tombol untuk melihat semua nilai HSL
         btn_view_all_hsl = tk.Button(self.root, text="Lihat Semua Nilai HSL", command=self.view_all_hsl)
         btn_view_all_hsl.pack(pady=5)
+
+        # Tombol untuk melihat semua nilai Hex
+        btn_view_all_hex = tk.Button(self.root, text="Lihat Semua Nilai Hex", command=self.view_all_hex)
+        btn_view_all_hex.pack(pady=5)
+
+        # Tombol untuk melihat histogram setiap kanal(channels) warna
+        btn_view_rgb_hist = tk.Button(self.root, text="Lihat Histogram Citra Berwarna", command=self.view_rgb_hist)
+        btn_view_rgb_hist.pack(pady=5)
+
+        # Tombol untuk melihat histogram citra grayscale
+        btn_view_grayscale_hist = tk.Button(self.root, text="Lihat Histogram Citra Grayscale", command=self.view_grayscale_hist)
+        btn_view_grayscale_hist.pack(pady=5)
 
     def open_image(self):
         """Fungsi untuk membuka dan menampilkan gambar yang dipilih pengguna."""
@@ -106,6 +122,9 @@ class ImageRGBViewerApp:
                 # Konversi RGB ke HSL
                 hsl = self.rgb_to_hsl(*rgb)
                 self.label_hsl.config(text=f"HSL pada ({x}, {y}): {hsl}")
+
+                hex_rgb = "#" + "".join([hex(x).replace('0x', '').upper() for x in rgb])
+                self.label_hex.config(text=f"Hex pada ({x}, {y}): {hex_rgb}")
             else:
                 messagebox.showerror("Error", "Koordinat di luar batas gambar.")
                 
@@ -168,11 +187,133 @@ class ImageRGBViewerApp:
         # Masukkan semua data ke dalam scrolled text
         scroll_text.insert(tk.END, pixels_hsl)
 
+    def view_all_hex(self):
+        """Fungsi untuk menampilkan semua nilai Hex dalam bentuk matriks."""
+        if not self.img:
+            messagebox.showerror("Error", "Silakan unggah gambar terlebih dahulu.")
+            return
+
+        # Membuat jendela baru untuk menampilkan nilai Hex
+        hex_window = tk.Toplevel(self.root)
+        hex_window.title("Semua Nilai Hex")
+
+        # Textbox dengan scroll untuk menampilkan nilai Hex
+        scroll_text = scrolledtext.ScrolledText(hex_window, wrap=tk.WORD, width=100, height=30)
+        scroll_text.pack(padx=10, pady=10)
+
+        # Gunakan load() untuk mengakses pixel secara lebih cepat
+        img_data = self.img.load()
+
+        # Gunakan list comprehension untuk menggabungkan data Hex lebih cepat
+        pixels_hex = "\n".join(
+                " ".join(
+                    f"#{''.join([hex(c).replace('0x', '').zfill(2).upper() for c in img_data[x, y]])}" 
+                    for x in range(self.img.width)  
+                )
+                for y in range(self.img.height) 
+            )
+
+        # Masukkan semua data ke dalam scrolled text
+        scroll_text.insert(tk.END, pixels_hex)    
+
     def rgb_to_hsl(self, r, g, b):
         """Konversi dari RGB ke HSL. Input dalam rentang 0-255, output HSL dalam rentang (0-360, 0-100, 0-100)."""
         r, g, b = [x / 255.0 for x in (r, g, b)]  # Normalisasi RGB ke rentang 0-1
         h, l, s = colorsys.rgb_to_hls(r, g, b)
         return int(h * 360), int(s * 100), int(l * 100)
+
+    def view_rgb_hist(self):
+        """Fungsi untuk menampilkan histogram citra berwarna (RGB)."""
+        if not self.img:
+            messagebox.showerror("Error", "Silakan unggah gambar terlebih dahulu.")
+            return
+
+        # Membuat jendela baru untuk menampilkan histogram
+        rgb_hist_window = tk.Toplevel(self.root)
+        rgb_hist_window.title("Histogram Citra Berwarna")
+
+        # Gunakan getdata() untuk mengakses pixel secara lebih cepat
+        img_rgb = list(self.img.getdata())
+        
+        # Pisahkan intensitas setiap channel warna
+        red_channel = [pixel[0] for pixel in img_rgb]
+        green_channel = [pixel[1] for pixel in img_rgb]
+        blue_channel = [pixel[2] for pixel in img_rgb]
+
+        # Membuat figure untuk menampilkan histogram
+        fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(6, 8), constrained_layout=True)
+
+        # Plot histogram untuk masing-masing channel
+        axs[0].hist(red_channel, bins=256, color='red', alpha=0.7)
+        axs[0].set_title("Red Channel")
+        axs[0].set_xlabel("Intensity")
+        axs[0].set_ylabel("Frequency")
+
+        axs[1].hist(green_channel, bins=256, color='green', alpha=0.7)
+        axs[1].set_title("Green Channel")
+        axs[1].set_xlabel("Intensity")
+        axs[1].set_ylabel("Frequency")
+
+        axs[2].hist(blue_channel, bins=256, color='blue', alpha=0.7)
+        axs[2].set_title("Blue Channel")
+        axs[2].set_xlabel("Intensity")
+        axs[2].set_ylabel("Frequency")
+
+        # Menampilkan histogram di tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=rgb_hist_window)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+        # Fungsi untuk menutup jendela histogram
+        def on_close():
+            canvas.get_tk_widget().pack_forget()  # Hapus widget canvas dari Tkinter
+            canvas._tkcanvas.destroy()  # Hapus canvas dari Tkinter
+            plt.close(fig)  # Tutup figure matplotlib
+            rgb_hist_window.destroy()  # Tutup jendela Tkinter
+
+        # Tambahkan handler untuk event tutup jendela
+        rgb_hist_window.protocol("WM_DELETE_WINDOW", on_close)
+
+    def view_grayscale_hist(self):
+        """Fungsi untuk menampilkan histogram citra grayscale."""
+        if not self.img:
+            messagebox.showerror("Error", "Silakan unggah gambar terlebih dahulu.")
+            return
+
+        # Membuat jendela baru untuk menampilkan histogram
+        gray_hist_window = tk.Toplevel(self.root)
+        gray_hist_window.title("Histogram Citra Grayscale")
+
+        # Gunakan convert() untuk mengubah gambar menjadi grayscale
+        gray_img = self.img.convert("L")
+
+        # Ambil data piksel dari gambar grayscale
+        gray_data = list(gray_img.getdata())
+        
+
+        # Membuat figure untuk menampilkan histogram
+        fig, ax = plt.subplots(figsize=(6, 4))
+
+        # Plot histogram untuk channel grayscale
+        ax.hist(gray_data, bins=256, color='gray', alpha=0.7)
+        ax.set_title("Grayscale Histogram")
+        ax.set_xlabel("Intensity")
+        ax.set_ylabel("Frequency")
+
+        # Menampilkan histogram di tkinter window
+        canvas_figure = FigureCanvasTkAgg(fig, master=gray_hist_window)
+        canvas_figure.draw()
+        canvas_figure.get_tk_widget().pack()
+
+        # Fungsi untuk menutup jendela histogram
+        def on_close():
+            canvas_figure.get_tk_widget().pack_forget() 
+            canvas_figure._tkcanvas.destroy() 
+            plt.close(fig) 
+            gray_hist_window.destroy()  
+
+        # Tambahkan handler untuk event tutup jendela
+        gray_hist_window.protocol("WM_DELETE_WINDOW", on_close)   
 
 # Jalankan aplikasi
 if __name__ == "__main__":
